f3dff9d6964adae568c4352d652ebc8e3ba25edf
manuamsler <127770114+manuamsler@users.noreply.github.com>
2023-03-15 14:34:15 -0300

    Update Prode.java

--- Prode/src/main/java/argprog157/prode/Prode.java
+++ Prode/src/main/java/argprog157/prode/Prode.java
@@ -9,5 +9,7 @@ public class Prode {
 
     public static void main(String[] args) {
         System.out.println("Argentina Programa 4.0 \nDesarrollador JAVA inicial \nComisión 157 - Grupo 5 \nTrabajo Integrador");
+                System.out.println("hola github");//agregado 14:27hs
+
     }
 }
