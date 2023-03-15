e11571a0e40937cd99fe5cf8fd48d55079eec995
Marcelo Ranzani <raniten@gmail.com>
2023-03-15 12:37:53 -0300

    Creación del proyecto

---
+++ Prode/src/main/java/argprog157/prode/Prode.java
@@ -0,0 +1,13 @@
+
+package argprog157.prode;
+
+/**
+ *
+ * @author Argentina Programa - Comisión 157-2023 - Grupo 5
+ */
+public class Prode {
+
+    public static void main(String[] args) {
+        System.out.println("Argentina Programa 4.0 \nDesarrollador JAVA inicial \nComisión 157 - Grupo 5 \nTrabajo Integrador");
+    }
+}
