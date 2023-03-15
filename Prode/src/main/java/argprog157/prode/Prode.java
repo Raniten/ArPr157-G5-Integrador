3d176d84b6c2d7d493c5541a699ecc5c7e33854f
Marcelo Ranzani <raniten@gmail.com>
2023-03-15 16:09:46 -0300

    Se agregaron los archivos de prueba csv, el de pronosticos y el de resultados

--- Prode/src/main/java/argprog157/prode/Prode.java
+++ Prode/src/main/java/argprog157/prode/Prode.java
@@ -8,10 +8,13 @@ package argprog157.prode;
 public class Prode {
 
     public static void main(String[] args) {
-        System.out.println("Argentina Programa 4.0 \nDesarrollador JAVA inicial \nComisión 157 - Grupo 5 \nTrabajo Integrador");
+        System.out.println("Argentina Programa 4.0 - Desarrollador JAVA inicial \nComisión 157 - Grupo 5 - Trabajo Integrador");
         System.out.println("hola github");//agregado 14:27hs x Manu Amsler
         System.out.println("java17.0.6, sino, no anda");//agregado 14:27hs x Manu Amsler
-
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Primer paso antes de trabajar, \nen el desktop, hacer un fetch, \nsi hay cambios en el proyecto, hacer un pull"); 
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Cuando se termina de trabajar, guardar, cerrar, \ny en el desktop, hacer un commit agregando comentarios sino no deja, \ny luego el push"); //agregado pro Marcelo Ranzani
 
     }
 }
