31d951447388efba25c5213ac5e20dcec0d84178
Marcelo Ranzani <raniten@gmail.com>
2023-03-19 19:15:17 -0300

    Se corrigió un error en la creación del ID de Partido y de Pronostico. Se corrigió un numero de equipo en pronostico.csv

--- Prode/src/main/java/argprog157/prode/CSVtoProno.java
+++ Prode/src/main/java/argprog157/prode/CSVtoProno.java
@@ -1,4 +1,3 @@
-
 package argprog157.prode;
 
 import java.util.ArrayList;
@@ -8,42 +7,53 @@ import java.util.ArrayList;
  * @author Argentina Programa - Comisión 157-2023 - Grupo 5
  */
 public class CSVtoProno {
+
     ArrayList<String[]> datos = new ArrayList<>();
     ArrayList<Pronostico> misPronosticos = new ArrayList<>();
-    
-    public CSVtoProno (ArrayList<String[]> datos ){
+
+    public CSVtoProno(ArrayList<String[]> datos) {
         this.datos = datos;
     }
-    
+
     public ArrayList<Pronostico> getArrayListProno() {
-        
+
         for (String[] fila : this.datos) {
             Pronostico prono1 = new Pronostico();
-            
+
             prono1.setEq1Id(Integer.parseInt(fila[0]));
-            if(fila[1].equals("X")) {
+            if (fila[1].equals("X")) {
                 prono1.setGana1(true);
             }
-            if(fila[2].equals("X")) {
+            if (fila[2].equals("X")) {
                 prono1.setEmpatan(true);
             }
-            if(fila[3].equals("X")) {
+            if (fila[3].equals("X")) {
                 prono1.setGana2(true);
             }
             prono1.setEq2Id(Integer.parseInt(fila[4]));
-            
-            if(prono1.getEq1Id() < prono1.getEq2Id()) {
-                if(prono1.getEq1Id() < 10) {
-                    if(prono1.getEq2Id() < 10) {
-                        prono1.setPronosticoId("0" + prono1.getEq1Id() + "0" +  prono1.getEq2Id());
+
+            if (prono1.getEq1Id() < prono1.getEq2Id()) {  //El ID del equipo 1 es menor al ID del equipo 2
+                if (prono1.getEq1Id() < 10) {
+                    if (prono1.getEq2Id() < 10) {
+                        prono1.setPronosticoId("0" + prono1.getEq1Id() + "0" + prono1.getEq2Id());
                     } else {
-                        prono1.setPronosticoId("0" + prono1.getEq2Id() + prono1.getEq1Id());
+                        prono1.setPronosticoId("0" + prono1.getEq1Id() + prono1.getEq2Id());
                     }
                 } else if (prono1.getEq2Id() < 10) {
-                        prono1.setPronosticoId("" + prono1.getEq1Id() + "0" +  prono1.getEq2Id());
-                    } else {
-                        prono1.setPronosticoId("" + prono1.getEq1Id() + prono1.getEq2Id());
-                    }
+                    prono1.setPronosticoId("" + prono1.getEq1Id() + "0" + prono1.getEq2Id());
+                } else {
+                    prono1.setPronosticoId("" + prono1.getEq1Id() + prono1.getEq2Id());
+                }
+            } else if (prono1.getEq2Id() < 10) {  //El ID del segundo equipo es menor al ID del primer equipo, están al revés
+                if (prono1.getEq1Id() < 10) {
+                    prono1.setPronosticoId("0" + prono1.getEq2Id() + "0" + prono1.getEq1Id());
+                } else {
+                    prono1.setPronosticoId("0" + prono1.getEq2Id() + prono1.getEq1Id());
+                }
+            } else if (prono1.getEq1Id() < 10) {
+                prono1.setPronosticoId("" + prono1.getEq2Id() + "0" + prono1.getEq1Id());
+            } else {
+                prono1.setPronosticoId("" + prono1.getEq2Id() + prono1.getEq1Id());
             }
 
             this.misPronosticos.add(prono1); //Agregamos un elemento del tipo PRONOSTICO al ArrayList
--- Prode/src/main/java/argprog157/prode/CSVtoResul.java
+++ Prode/src/main/java/argprog157/prode/CSVtoResul.java
@@ -6,36 +6,46 @@ import java.util.ArrayList;
  * @author Argentina Programa - Comisión 157-2023 - Grupo 5
  */
 public class CSVtoResul {
-    
+
     ArrayList<String[]> datos = new ArrayList<String[]>();
     ArrayList<Resultado> misResultados = new ArrayList<Resultado>();
-    
-    public CSVtoResul (ArrayList<String[]> datos ){
+
+    public CSVtoResul(ArrayList<String[]> datos) {
         this.datos = datos;
     }
-    
+
     public ArrayList<Resultado> getArrayListResul() {
-        
+
         for (String[] fila : this.datos) {
             Resultado resul1 = new Resultado();
             resul1.setEq1Id(Integer.parseInt(fila[0]));
             resul1.setGolesEq1(Integer.parseInt(fila[3]));
             resul1.setGolesEq2(Integer.parseInt(fila[4]));
             resul1.setEq2Id(Integer.parseInt(fila[5]));
-            if(resul1.getEq1Id() < resul1.getEq2Id()) {
-                if(resul1.getEq1Id() < 10) {
-                    if(resul1.getEq2Id() < 10) {
-                        resul1.setResultadoId("0" + resul1.getEq1Id() + "0" +  resul1.getEq2Id());
+            if (resul1.getEq1Id() < resul1.getEq2Id()) {
+                if (resul1.getEq1Id() < 10) {
+                    if (resul1.getEq2Id() < 10) {
+                        resul1.setResultadoId("0" + resul1.getEq1Id() + "0" + resul1.getEq2Id());
+                    } else {
+                        resul1.setResultadoId("0" + resul1.getEq1Id() + resul1.getEq2Id());
+                    }
+                } else if (resul1.getEq2Id() < 10) {
+                    resul1.setResultadoId("" + resul1.getEq1Id() + "0" + resul1.getEq2Id());
+                } else {
+                    resul1.setResultadoId("" + resul1.getEq1Id() + resul1.getEq2Id());
+                }
+            } else if (resul1.getEq2Id() < 10) {
+                    if (resul1.getEq1Id() < 10) {
+                        resul1.setResultadoId("0" + resul1.getEq2Id() + "0" + resul1.getEq1Id());
                     } else {
                         resul1.setResultadoId("0" + resul1.getEq2Id() + resul1.getEq1Id());
                     }
-                } else if (resul1.getEq2Id() < 10) {
-                        resul1.setResultadoId("" + resul1.getEq1Id() + "0" +  resul1.getEq2Id());
-                    } else {
-                        resul1.setResultadoId("" + resul1.getEq1Id() + resul1.getEq2Id());
-                    }
-            }
-            
+                } else if (resul1.getEq1Id() < 10) {
+                    resul1.setResultadoId("" + resul1.getEq2Id() + "0" + resul1.getEq1Id());
+                } else {
+                    resul1.setResultadoId("" + resul1.getEq2Id() + resul1.getEq1Id());
+                }
+
             this.misResultados.add(resul1); //Agregamos un elemento del tipo RESULTADO al ArrayList
         }
         return this.misResultados;
